337a10869702784c75af21bbd8f10d3669c71d07
amboss <silkenbeumerfynn@gmail.com>
2021-04-01 21:32:25 +0200

    Added setting of IP-Address for the Light. Added chalk. Added magic-home. Added body-parser. Added path.

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const express = require('express')
+const express = require('express');
+const { Control } = require('magic-home');
+const path = require('path');
+const chalk = require('chalk');
+const bodyParser = require('body-parser');
+
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.sendStatus(200);
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+let light = new Control("");
+console.log(light._address);
+
+app.get('/', function (req, res, next) {
+    if(!light._address){
+        res.sendFile(path.join(__dirname + '/setaddress.html'));
+        console.log(chalk.red(`IP Address not set yet. Sending setaddress.html form.`));
+    }else{
+        res.sendFile(path.join(__dirname + '/index.html'));
+    }
+})
+
+app.post('/setaddress', function (req, res, next) {
+    light._address = req.body.address;
+    console.log(chalk.green(`IP Address was set to: ${light._address}`));
+    res.redirect("/");
 })
  
 app.listen(80)
