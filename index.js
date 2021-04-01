44988e3fd3f6a6c7846ae032f37600ae80668adc
amboss <silkenbeumerfynn@gmail.com>
2021-04-01 22:43:15 +0200

    Added Colorchange.

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ const { Control } = require('magic-home');
 const path = require('path');
 const chalk = require('chalk');
 const bodyParser = require('body-parser');
+const utils = require('./utils')
 
 const app = express()
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
+app.use(bodyParser.urlencoded({ extended: true }));  
 
 let light = new Control("");
 console.log(light._address);
 
 app.get('/', function (req, res, next) {
     if(!light._address){
-        res.sendFile(path.join(__dirname + '/setaddress.html'));
+        res.sendFile(path.join(__dirname + '/web/setaddress.html'));
         console.log(chalk.red(`IP Address not set yet. Sending setaddress.html form.`));
     }else{
-        res.sendFile(path.join(__dirname + '/index.html'));
+        res.sendFile(path.join(__dirname + '/web/index.html'));
     }
 })
 
@@ -29,5 +28,22 @@ app.post('/setaddress', function (req, res, next) {
     console.log(chalk.green(`IP Address was set to: ${light._address}`));
     res.redirect("/");
 })
- 
+
+
+
+app.post('/setcolor', function (req, res, next) {
+    let color = utils.hexToRGB(req.body.color);
+    newColor(color.r, color.g, color.b);
+    console.log(chalk.green(`Color was set to: ${req.body.color}`));
+    res.redirect("/");
+})
+
+async function newColor(red, green, blue){
+    try{
+        await light.setColor(red, green, blue);
+    }catch{
+        
+    }
+}
+
 app.listen(80)
